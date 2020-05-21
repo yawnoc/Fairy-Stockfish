7af5b5012a6342ce5d200f8453a11ab321f0779b
Fabian Fichter <ianfab@users.noreply.github.com>
2020-05-21 22:39:13 +0200

    no mutual penalty for similar drops

--- src/search.cpp
+++ src/search.cpp
@@ -1710,6 +1710,7 @@ moves_loop: // When in check, search starts from here
         // Decrease all the non-best quiet moves
         for (int i = 0; i < quietCount; ++i)
         {
+            if (type_of(bestMove) != DROP || type_of(quietsSearched[i]) != DROP || to_sq(bestMove) != to_sq(quietsSearched[i]))
             thisThread->mainHistory[us][from_to(quietsSearched[i])] << -bonus2;
             update_continuation_histories(ss, pos.moved_piece(quietsSearched[i]), to_sq(quietsSearched[i]), -bonus2);
         }
